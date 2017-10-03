293abd420da0d200b007196519cbb9f695ce30dd
Dylan Bowman <bowma128@mail.chapman.edu>
2017-10-02 21:39:18 -0700

    Implements camera controller.

--- Assets/Scripts/Control/CameraController.cs
+++ Assets/Scripts/Control/CameraController.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    public GameObject toFollow;
+
+    private Vector3 oldLocation;
+
+    public Vector3 offset;
+
+    public Vector3 field;
+
 	// Use this for initialization
 	void Start () {
-		
+		if (toFollow == null) {
+            Debug.LogWarning("There is no object for the camera to follow.");
+        }
 	}
+
+    Vector3 vecAbs(Vector3 vec) {
+        return new Vector3(Mathf.Abs(vec.x), Mathf.Abs(vec.y), 0); //This is to project everything on the plane z = 0
+    }
+
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void LateUpdate () {
+        Vector3 diff = toFollow.transform.position - oldLocation;
+        transform.position = toFollow.transform.position + offset;
+
+
+
+        oldLocation = toFollow.transform.position;
 	}
 }
