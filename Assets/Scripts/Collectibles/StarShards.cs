0095ae8810832a6106b28c5b099787b0d363e019
Rabah Habiss <rhabiss@gmail.com>
2017-10-09 01:38:37 -0700

    Collectibles Per Level
    -Collectibles are now counted based on the number of star shard objects
    spawned in the level.
    -Added Scripts/LevelManager
    -Added UI label to show the number of star shards remaining.

--- Assets/Scripts/Collectibles/StarShards.cs
+++ Assets/Scripts/Collectibles/StarShards.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StarShards : MonoBehaviour {
+/*
+Name: Rabah Habiss
+ID: 2268381
+Email: habis102@mail.chapman.edu
+Course: CPSC-340-01
+Assignment: Semester Project
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+Description: Collectible Script. Attach to star shard objects
+*/
+
+public class StarShards : MonoBehaviour
+{
+
+    private bool isCollected = false;
+    private Renderer render;
+    private LevelManager level;
+
+    // Use this for initialization
+    void Start()
+    {
+        level = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+        render = GetComponent<Renderer>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !isCollected)
+        {
+            isCollected = true;
+            render.enabled = false;
+            level.CollectShard();
+        }
+    }
+
 }
