aed01560fb042ca9a0f4c0714cf9c2c7a6211919
Dylan Bowman <dbowman06@gmail.com>
2017-10-11 21:12:36 -0700

    Revert "Collectibles Per Level"
    This reverts commit 0095ae8810832a6106b28c5b099787b0d363e019.

--- Assets/Scripts/Control/PlayerMovementController.cs
+++ Assets/Scripts/Control/PlayerMovementController.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class PlayerMovementController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
 
     private CharacterController controller;
+    private int shardCount;
+    private int collectWin = 2;  //temp value - must inheret from global config
 
     // Use this for initialization
     void Start() {
@@ -17,6 +20,7 @@ public class PlayerMovementController : MonoBehaviour {
             Debug.LogError("There's no Character controller on: " + transform.name);
         }
 
+        SetCountText();
     }
 
     // Update is called once per frame
@@ -37,5 +41,26 @@ public class PlayerMovementController : MonoBehaviour {
 
         controller.Move(movement * speed * Time.deltaTime);
     }
+
+    //Collect star shards
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("StarShard"))
+        {
+            collision.gameObject.SetActive(false);
+            shardCount++;
+            SetCountText();
+
+        }
+    }
+
+    //Collectibles Counter
+    void SetCountText()
+    {
+        countText.text = "Star shards: " + shardCount.ToString();
+        if (shardCount >= collectWin)
+            countText.color = Color.green;         
+    }
+
 
 }
