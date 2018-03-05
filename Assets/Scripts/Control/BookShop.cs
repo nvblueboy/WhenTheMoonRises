43e6cba32a883f991edf08299768f47800b9eaf4
Andrew <krage100@mail.chapman.edu>
2018-03-05 13:37:52 -0800

    BookStore

--- Assets/Scripts/Control/BookShop.cs
+++ Assets/Scripts/Control/BookShop.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -12,9 +11,9 @@ Email: krage100@mail.chapman.edu
 Course: CPSC-440-01
 Assignment: Semester Project
 
-Description: This is a script controlling the in game shop
+Description: This is a script controlling the in game book shop
 */
-public class ItemShop : MonoBehaviour
+public class BookShop : MonoBehaviour
 {
     public GameObject arrow1;
     public GameObject arrow2;
@@ -24,21 +23,15 @@ public class ItemShop : MonoBehaviour
     public GameObject arrow6;
     public GameObject arrow7;
     public GameObject arrow8;
-    public GameObject arrow9;
-    public GameObject arrow10;
-    public GameObject arrow11;
     int placeholder = 0;
     //ArrayList arrows = new ArrayList();
-    public Button smallHP;
-    public Button medHP;
-    public Button largeHP;
-    public Button smallST;
-    public Button medST;
-    public Button largeST;
-    public Button fireCrack;
-    public Button sBomb;
-    public Button aoeBomb;
-    public Button bundle;
+    public Button maxHP;
+    public Button stamina;
+    public Button strength;
+    public Button defense;
+    public Button intuition;
+    public Button magic;
+    public Button luck;
     public Button exit;
     public Text shopText;
     public Text coinText;
@@ -59,15 +52,9 @@ public class ItemShop : MonoBehaviour
         arrow6 = GameObject.Find("arrow6");
         arrow7 = GameObject.Find("arrow7");
         arrow8 = GameObject.Find("arrow8");
-        arrow9 = GameObject.Find("arrow9");
-        arrow10 = GameObject.Find("arrow10");
-        arrow11 = GameObject.Find("arrow11");
         player.increaseCoins(1000);
         coins = player.getCoins();
         coinText.text = (player.getCoins()).ToString() + " C";
-        //arrows.Add(arrow1);
-        //arrows.Add(arrow2);
-        //arrows.Add(arrow3);
     }
     void Start()
     {
@@ -79,10 +66,7 @@ public class ItemShop : MonoBehaviour
         arrow6.SetActive(false);
         arrow7.SetActive(false);
         arrow8.SetActive(false);
-        arrow9.SetActive(false);
-        arrow10.SetActive(false);
-        arrow11.SetActive(false);
-        itemInfo.text = "Restores 5 HP ";
+        itemInfo.text = "+1 Max HP ";
     }
     // Update is called once per frame
     void Update()
@@ -91,69 +75,51 @@ public class ItemShop : MonoBehaviour
         {
             if (arrow1.activeInHierarchy)
             {
-                itemInfo.text = "Restores 20 HP ";
+                itemInfo.text = "+1 Stamina ";
                 arrow1.SetActive(false);
                 arrow2.SetActive(true);
             }
             else if (arrow2.activeInHierarchy)
             {
-                itemInfo.text = "Restores 50 HP ";
+                itemInfo.text = "+1 Strength ";
                 arrow2.SetActive(false);
                 arrow3.SetActive(true);
             }
             else if (arrow3.activeInHierarchy)
             {
-                itemInfo.text = "Restores 5 Stamina ";
+                itemInfo.text = "+1 Defense ";
                 arrow3.SetActive(false);
                 arrow4.SetActive(true);
             }
             else if (arrow4.activeInHierarchy)
             {
-                itemInfo.text = "Restores 20 Stamina ";
+                itemInfo.text = "+1 Intuition ";
                 arrow4.SetActive(false);
                 arrow5.SetActive(true);
             }
             else if (arrow5.activeInHierarchy)
             {
-                itemInfo.text = "Restores 50 Stamina ";
+                itemInfo.text = "+1 Magic ";
                 arrow5.SetActive(false);
                 arrow6.SetActive(true);
 
             }
             else if (arrow6.activeInHierarchy)
             {
-                itemInfo.text = "-5 Enemy HP, Burned Status Effect ";
+                itemInfo.text = "+1 Luck ";
                 arrow6.SetActive(false);
                 arrow7.SetActive(true);
             }
             else if (arrow7.activeInHierarchy)
             {
-                itemInfo.text = "-3 Enemy HP \n to 1 Enemy ";
+                itemInfo.text = " ";
                 arrow7.SetActive(false);
                 arrow8.SetActive(true);
             }
             else if (arrow8.activeInHierarchy)
             {
-                itemInfo.text = "-3 Enemy HP to All Enemies ";
+                itemInfo.text = "+1 to Maximum HP ";
                 arrow8.SetActive(false);
-                arrow9.SetActive(true);
-            }
-            else if (arrow9.activeInHierarchy)
-            {
-                itemInfo.text = "-8 Enemy HP to All Enemies ";
-                arrow9.SetActive(false);
-                arrow10.SetActive(true);
-            }
-            else if (arrow10.activeInHierarchy)
-            {
-                itemInfo.text = "";
-                arrow10.SetActive(false);
-                arrow11.SetActive(true);
-            }
-            else if (arrow11.activeInHierarchy)
-            {
-                itemInfo.text = "Restores 5 HP ";
-                arrow11.SetActive(false);
                 arrow1.SetActive(true);
             }
         }
@@ -161,83 +127,65 @@ public class ItemShop : MonoBehaviour
         {
             if (arrow1.activeInHierarchy)
             {
-                itemInfo.text = "";
+                itemInfo.text = " ";
                 arrow1.SetActive(false);
-                arrow11.SetActive(true);
+                arrow8.SetActive(true);
             }
             else if (arrow2.activeInHierarchy)
             {
-                itemInfo.text = "Restores 5 HP ";
+                itemInfo.text = "+1 Max HP ";
                 arrow2.SetActive(false);
                 arrow1.SetActive(true);
             }
             else if (arrow3.activeInHierarchy)
             {
-                itemInfo.text = "Restores 20 HP ";
+                itemInfo.text = "+1 Stamina ";
                 arrow3.SetActive(false);
                 arrow2.SetActive(true);
             }
             else if (arrow4.activeInHierarchy)
             {
-                itemInfo.text = "Restores 50 HP ";
+                itemInfo.text = "+1 Strength ";
                 arrow4.SetActive(false);
                 arrow3.SetActive(true);
             }
             else if (arrow5.activeInHierarchy)
             {
-                itemInfo.text = "Restores 5 Stamina ";
+                itemInfo.text = "+1 Defense ";
                 arrow5.SetActive(false);
                 arrow4.SetActive(true);
 
             }
             else if (arrow6.activeInHierarchy)
             {
-                itemInfo.text = "Restores 20 Stamina ";
+                itemInfo.text = "+1 Intuition ";
                 arrow6.SetActive(false);
                 arrow5.SetActive(true);
             }
             else if (arrow7.activeInHierarchy)
             {
-                itemInfo.text = "Restores 50 Stamina ";
+                itemInfo.text = "+1 Magic ";
                 arrow7.SetActive(false);
                 arrow6.SetActive(true);
             }
             else if (arrow8.activeInHierarchy)
             {
-                itemInfo.text = "-5 Enemy HP, Burned Status Effect ";
+                itemInfo.text = "+1 Luck ";
                 arrow8.SetActive(false);
                 arrow7.SetActive(true);
-            }
-            else if (arrow9.activeInHierarchy)
-            {
-                itemInfo.text = "-3 Enemy HP \n to 1 Enemy ";
-                arrow9.SetActive(false);
-                arrow8.SetActive(true);
-            }
-            else if (arrow10.activeInHierarchy)
-            {
-                itemInfo.text = "-3 Enemy HP to All Enemies ";
-                arrow10.SetActive(false);
-                arrow9.SetActive(true);
-            }
-            else if (arrow11.activeInHierarchy)
-            {
-                itemInfo.text = "-8 Enemy HP to All Enemies ";
-                arrow11.SetActive(false);
-                arrow10.SetActive(true);
             }
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (arrow1.activeInHierarchy)
             {
-                if (coins >= 5)
+                if (coins >= 30)
                 {
-                    coins = coins - 5;
-                    smallHP.onClick.Invoke();
+                    coins = coins - 30;
+                    maxHP.onClick.Invoke();
                     //shopText.text = "You bought a small hp potion";
                     coinText.text = coins + " C";
-                    player.addItem(new Item("small hp potion", "small hp potion"));
+                    player.hp++;
                 }
                 else
                 {
@@ -246,13 +194,13 @@ public class ItemShop : MonoBehaviour
             }
             else if (arrow2.activeInHierarchy)
             {
-                if (coins >= 20)
+                if (coins >= 30)
                 {
-                    coins = coins - 20;
-                    medHP.onClick.Invoke();
+                    coins = coins - 30;
+                    stamina.onClick.Invoke();
                     //shopText.text = "You bought a medium hp potion";
                     coinText.text = coins + " C";
-                    player.addItem(new Item("medium hp potion", "medium hp potion"));
+                    player.stamina++;
                 }
                 else
                 {
@@ -261,13 +209,13 @@ public class ItemShop : MonoBehaviour
             }
             else if (arrow3.activeInHierarchy)
             {
-                if (coins >= 50)
+                if (coins >= 10)
                 {
-                    coins = coins - 50;
-                    largeHP.onClick.Invoke();
+                    coins = coins - 10;
+                    strength.onClick.Invoke();
                     //shopText.text = "You bought a large hp potion";
                     coinText.text = coins + " C";
-                    player.addItem(new Item("large hp potion", "large hp potion"));
+                    player.increaseStrength(1);
                 }
                 else
                 {
@@ -276,13 +224,13 @@ public class ItemShop : MonoBehaviour
             }
             else if (arrow4.activeInHierarchy)
             {
-                if (coins >= 5)
+                if (coins >= 10)
                 {
-                    coins = coins - 5;
-                    smallST.onClick.Invoke();
+                    coins = coins - 10;
+                    defense.onClick.Invoke();
                     //shopText.text = "You bought a small stamina potion";
                     coinText.text = coins + " C";
-                    player.addItem(new Item("small stamina potion", "small stamina potion"));
+                    player.increaseDefense(1);
                 }
                 else
                 {
@@ -294,10 +242,9 @@ public class ItemShop : MonoBehaviour
                 if (coins >= 20)
                 {
                     coins = coins - 20;
-                    medST.onClick.Invoke();
+                    intuition.onClick.Invoke();
                     //shopText.text = "You bought a medium stamina potion";
                     coinText.text = coins + " C";
-                    player.addItem(new Item("medium stamina potion", "medium stamina potion"));
                 }
                 else
                 {
@@ -306,13 +253,12 @@ public class ItemShop : MonoBehaviour
             }
             else if (arrow6.activeInHierarchy)
             {
-                if (coins >= 50)
+                if (coins >= 20)
                 {
-                    coins = coins - 50;
-                    largeST.onClick.Invoke();
+                    coins = coins - 20;
+                    magic.onClick.Invoke();
                     //shopText.text = "You bought a large stamina potion";
                     coinText.text = coins + " C";
-                    player.addItem(new Item("large stamina potion", "large stamina potion"));
                 }
                 else
                 {
@@ -321,13 +267,12 @@ public class ItemShop : MonoBehaviour
             }
             else if (arrow7.activeInHierarchy)
             {
-                if (coins >= 10)
+                if (coins >= 20)
                 {
-                    coins = coins - 10;
-                    fireCrack.onClick.Invoke();
+                    coins = coins - 20;
+                    luck.onClick.Invoke();
                     //shopText.text = "You bought a fire cracker";
                     coinText.text = coins + " C";
-                    player.addItem(new Item("fire cracker", "fire cracker"));
                 }
                 else
                 {
@@ -335,51 +280,6 @@ public class ItemShop : MonoBehaviour
                 }
             }
             else if (arrow8.activeInHierarchy)
-            {
-                if (coins >= 6)
-                {
-                    coins = coins - 6;
-                    sBomb.onClick.Invoke();
-                    //shopText.text = "You bought a single bomb";
-                    coinText.text = coins + " C";
-                    player.addItem(new Item("single bomb", "single bomb"));
-                }
-                else
-                {
-                    shopText.text = "You don't have enough \n coins to buy that";
-                }
-            }
-            else if (arrow9.activeInHierarchy)
-            {
-                if (coins >= 12)
-                {
-                    coins = coins - 12;
-                    aoeBomb.onClick.Invoke();
-                    //shopText.text = "You bought an aoe bomb";
-                    coinText.text = coins + " C";
-                    player.addItem(new Item("aoe bomb", "aoe bomb"));
-                }
-                else
-                {
-                    shopText.text = "You don't have enough \n coins to buy that";
-                }
-            }
-            else if (arrow10.activeInHierarchy)
-            {
-                if (coins >= 50)
-                {
-                    coins = coins - 50;
-                    bundle.onClick.Invoke();
-                    //shopText.text = "You bought a mega firework bundle";
-                    coinText.text = coins + " C";
-                    player.addItem(new Item("mega firework bundle", "mega firework bundle"));
-                }
-                else
-                {
-                    shopText.text = "You don't have enough \n coins to buy that";
-                }
-            }
-            else if (arrow11.activeInHierarchy)
             {
                 exit.onClick.Invoke();
             }
@@ -388,4 +288,3 @@ public class ItemShop : MonoBehaviour
 
     }
 }
-
