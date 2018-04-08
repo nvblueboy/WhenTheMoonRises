5afeb5c8a32558d05645c27c678df662d71d2a71
Rabah Habiss <rhabiss@gmail.com>
2018-04-07 21:01:37 -0700

    Fixes Bookstore scene

---
+++ Assets/Scripts/FullscreenBG.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullscreenBG : MonoBehaviour
+{
+
+    void Awake()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        Vector2 cameraSize = new Vector2(Camera.main.aspect * cameraHeight, cameraHeight);
+        Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
+
+        Vector2 scale = transform.localScale;
+        if (cameraSize.x >= cameraSize.y)
+        {
+            scale = scale*(cameraSize.x / spriteSize.x);
+        }
+        else
+        {
+            scale = scale*(cameraSize.y / spriteSize.y);
+        }
+
+        transform.position = Vector2.zero;
+        transform.localScale = scale;
+    }
+}
+
