6843b1de8e070b573601c7f941721653a800b0ca
Dylan Bowman <dbowman06@gmail.com>
2018-05-03 17:26:13 -0700

    Adds all sorts of stuff

--- Assets/Scripts/Control/TitleScreenController.cs
+++ Assets/Scripts/Control/TitleScreenController.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleScreenController : MonoBehaviour {
 
     private float oldJump;
     private bool jumpFrame;
+
+    public Text title;
+    public Text pressSpace;
+    public Text copyright;
+
+    private Color space_baseColor;
+    private Color title_baseColor;
+    private Color copyright_baseColor;
+
+    float startTime;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +28,11 @@ public class TitleScreenController : MonoBehaviour {
         if (go != null) {
             Destroy(go);
         }
+
+        space_baseColor = pressSpace.color;
+        title_baseColor = title.color;
+        copyright_baseColor = copyright.color;
+        startTime = Time.time;
 	}
 	
 	// Update is called once per frame
@@ -34,5 +50,29 @@ public class TitleScreenController : MonoBehaviour {
         if (jumpFrame) {
             SceneManager.LoadScene("Day1");
         }
+
+        float currTime = Time.time - startTime;
+
+        float runTime = 2;
+
+        float mult = 1f;
+        if (currTime < runTime) {
+            mult = 0;
+        }else if (currTime < runTime * 2) {
+            mult = (1 / runTime) * (currTime - runTime) ;
+        }
+
+        Color c = space_baseColor;
+        c.a = (Mathf.Sin(Time.time * 2)/5 + .8f) * mult;
+        pressSpace.color = c;
+
+        c = title_baseColor;
+        c.a = title_baseColor.a * mult;
+        title.color = c;
+
+        c = copyright_baseColor;
+        c.a = copyright_baseColor.a * mult;
+        copyright.color = c;
+
     }
 }
